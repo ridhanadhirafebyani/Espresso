658c7ec8536eb91ddbbccb92c2e74c04866b315d
ridhanadhirafebyani <ridha_febyani_24rpl@student.smktelkom-mlg.sch.id>
2018-02-12 14:02:56 +0700

    AddIdlingResourceMenuActivityTest

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -5,20 +5,17 @@ package com.example.android.teatime;
  */
 
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.anything;
 
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
@@ -53,10 +50,10 @@ public class MenuActivityScreenTest {
 
         // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
         // gridview item and clicks it.
-        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(1).perform(click());
+        Espresso.onData(Matchers.anything()).inAdapterView(ViewMatchers.withId(R.id.tea_grid_view)).atPosition(1).perform(ViewActions.click());
 
         // Checks that the OrderActivity opens with the correct tea name displayed
-        onView(withId(R.id.tea_name_text_view)).check(matches(withText(TEA_NAME)));
+        Espresso.onView(ViewMatchers.withId(R.id.tea_name_text_view)).check(ViewAssertions.matches(ViewMatchers.withText(TEA_NAME)));
 
 
     }
